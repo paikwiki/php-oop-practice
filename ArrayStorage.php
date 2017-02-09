8b02220dcf6ccbe7e50b4a6838ada1cd287df06b
paikwiki <paikwiki@gmail.com>
2017-02-09 11:12:50 +0900

    Complete daily practice-20170209

--- ArrayStorage.php
+++ ArrayStorage.php
@@ -1,5 +1,18 @@
 <?php
 
-class ArrayStorage
+require 'StorageInterface.php';
+
+class ArrayStorage implements StorageInterface
 {
+    protected $collectoion = [];
+
+    public function put($item)
+    {
+        $this->collection[] = $item;
+    }
+
+    public function collection()
+    {
+        return $this->collection;
+    }
 }
--- Post.php
+++ Post.php
@@ -1,5 +1,43 @@
 <?php
 
-class Post
+require 'Writing.php';
+//require 'ArrayStorage.php';
+require 'FileStorage.php';
+
+class Post extends Writing
 {
+    public function save()
+    {
+        $this->storage->put([
+            'model' => __CLASS__,
+            'title' => $this->title,
+        ]);
+    }
 }
+
+$storage = new FileStorage;
+(new Post('Altus elevatus saepe resuscitabos eleates est.', $storage))->save();
+(new Post('Nunquam resuscitabo lanista.', $storage))->save();
+var_dump($storage->collection());
+
+//$storage = new ArrayStorage;
+//(new Post('Calcaria congregabos, tanquam teres barcas.', $storage))->save();
+//(new Post('Brevis exemplars ducunt ad nix.', $storage))->save();
+//var_dump($storage->collection());
+
+//(new Post('Cur xiphias prarere?'))->save();
+
+//$post = new Post('Naviss observare!');
+//echo $post->getTitle();
+
+//$post = new Post('Sunt brabeutaes imperium ferox, fortis caculaes.');
+//$post->increaseViewCount();
+//$post->increaseViewCount();
+//var_dump($post);
+
+//$post = new Post('Sunt brabeutaes imperium ferox, fortis caculaes.');
+//echo $post->title;
+
+//$post = new Post;
+//$post->title = 'Sunt mensaes experientia noster, albus scutumes.';
+//echo $post->title;
