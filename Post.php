6ac07594da0be068db5547a0e505423326294da4
paikwiki <paikwiki@gmail.com>
2017-02-11 16:00:52 +0900

    Complete daily practice-20170209

--- Post.php
+++ Post.php
@@ -1,5 +1,23 @@
 <?php
 
-class Post
+require 'Writing.php';
+//require 'ArrayStorage.php';
+require 'FileStorage.php';
+
+class Post extends Writing
 {
+    function save()
+    {
+        $this->storage->put([
+            'model' => __CLASS__,
+            'title' => $this->title,
+        ]);
+    }
 }
+
+//$storage = new ArrayStorage;
+$storage = new FileStorage;
+
+(new Post('Nunquam anhelare caesium.', $storage))->save();
+(new Post('Cur lacta mori.', $storage))->save();
+var_dump($storage->collection());
--- Writing.php
+++ Writing.php
@@ -2,4 +2,41 @@
 
 abstract class Writing
 {
+    protected $title;
+    protected $storage;
+    protected $viewCount = 0;
+
+    public function __construct($title, StorageInterface $storage)
+    {
+        $this->setTitle($title);
+        $this->storage = $storage;
+    }
+
+    /**
+     * @param mixed $title
+     * @throws Exception
+     */
+    protected function setTitle($title)
+    {
+        if(strlen($title)<10)
+        {
+            throw new Exception('10글자보다 길게 입력하세요');
+        }
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return ucfirst($this->title);
+    }
+
+    public function increaseViewCount()
+    {
+        $this->viewCount += 1;
+    }
+
+    abstract function save();
 }
