d97f4516143e801d9b56ec564bd425f25d96a52d
paikwiki <paikwiki@gmail.com>
2017-02-20 11:14:26 +0900

    Complete daily practice-20170220

--- Post.php
+++ Post.php
@@ -1,5 +1,19 @@
 <?php
+require 'Writing.php';
+require 'FileStorage.php';
 
-class Post
+class Post extends Writing
 {
+    public function save()
+    {
+        $this->storage->put([
+            'model' => __CLASS__,
+            'title' => $this->title,
+        ]);
+    }
 }
+
+$storage = new FileStorage;
+(new Post('Est magnum advena, cesaris!', $storage))->save();
+(new Post('Est brevis musa, cesaris!', $storage))->save();
+var_dump($storage->collection());
