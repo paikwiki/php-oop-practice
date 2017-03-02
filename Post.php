98ff3b80753eb51277e28281e774ae43e5229081
paikwiki <paikwiki@gmail.com>
2017-03-02 09:02:05 +0900

    Complete daily practice-20170302

--- Post.php
+++ Post.php
@@ -1,5 +1,20 @@
 <?php
+require 'Writing.php';
+require 'FileStorage.php';
 
-class Post
+class Post extends Writing
 {
+    public function save()
+    {
+        $this->storage->put([
+            'model' => __CLASS__,
+            'title' => $this->title,
+        ]);
+    }
 }
+
+$storage = new FileStorage;
+(new Post('Accentor, imber, et brodium.', $storage))->save();
+(new Post('Ubi est germanus cobaltum.', $storage))->save();
+
+var_dump($storage->collection());
